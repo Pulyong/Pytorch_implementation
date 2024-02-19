57956bdcec8eee797951a6eea0d0bfe8bc64cfa2
Pulyong <rla020@naver.com>
2024-02-19 21:01:58 +0900

    update gn In

--- norm/groupnorm.py
+++ norm/groupnorm.py
@@ -1,17 +1,21 @@
 import torch
 from torch import nn
 
-class InstanceNorm(nn.Module):
-    def __init__(self, channels: int, *,
+class GroupNorm(nn.Module):
+    def __init__(self, groups: int, channels: int, *,
                  eps: float = 1e-5, momentum: float = 0.1,
                  affine: bool = True, track_running_stats: bool = False):
+        
         super().__init__()
-
+        
+        assert channels % groups == 0, "Number of channels should be evenly divisible by the number of groups"
+        self.groups = groups
         self.channels = channels
+
+        self.track_running_stats = track_running_stats
+        self.momentum = momentum
         self.eps = eps
         self.affine = affine
-        self.momentum = momentum
-        self.track_running_stats = track_running_stats
 
         if self.affine:
             self.scale = nn.Parameter(torch.ones(channels))
@@ -22,14 +26,13 @@ class InstanceNorm(nn.Module):
             self.register_buffer('exp_var', None)
 
     def forward(self, x: torch.Tensor):
+        
         x_shape = x.shape
-
         batch_size = x_shape[0]
+        assert self.channels == x.shape[1]
 
-        assert self.channels == x_shape[1]
-
-        x = x.view(batch_size,self.channels,-1)
-
+        x = x.view(batch_size,self.groups,-1)
+        
         if self.training or not self.track_running_stats: # test 단계에서 running average를 사용하지 않아도 진입(mean, var 계산을 위해)
             mean = x.mean(dim=[-1],keepdim=True)
             mean_x2 = (x**2).mean(dim=[-1],keepdim=True)
@@ -47,19 +50,19 @@ class InstanceNorm(nn.Module):
             mean = self.exp_mean
             var = self.exp_var
 
-
-        x_norm = (x-mean) / torch.sqrt(var+self.eps)
-        x_norm - x_norm.view(batch_size,self.channels,-1)
+        x_norm = (x-mean) / torch.sqrt(var + self.eps)
+        
 
         if self.affine:
+            x_norm = x_norm.view(batch_size, self.channels, -1)
             x_norm = self.scale.view(1,-1,1)*x_norm + self.shift.view(1,-1,1)
 
         return x_norm.view(x_shape)
-
+    
 if __name__=='__main__':
     x = torch.zeros([2, 6, 2, 4])
     print(x.shape)
-    bn = InstanceNorm(6,track_running_stats=True)
+    bn = GroupNorm(2,6,track_running_stats=True)
 
     x = bn(x)
     print(x.shape)
